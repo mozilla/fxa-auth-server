8fc9a4ead4ff940ebc6d1a55c1c6e3a05b343998
Andrew Chilton <andychilton@gmail.com>
2014-03-11 17:57:29 +1300

    Fixes #262 : Add background job for pruning old tokens

--- bin/key_server.js
+++ bin/key_server.js
@@ -63,7 +63,7 @@ function main() {
           Token.PasswordChangeToken
         )
 
-        DB.connect(config[config.db.backend])
+        DB.connect(config)
           .done(
             function (db) {
               var routes = require('../routes')(log, error, serverPublicKey, signer, db, mailer, config)
@@ -80,7 +80,6 @@ function main() {
               process.exit(1)
             }
           )
-
       }
     )
 
