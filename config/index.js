f322aab302ff548bc4bec47cf90fb595c9f1dc8a
Vijay Budhram <vbudhram@mozilla.com>
2016-11-07 11:27:48 -0500

    feat(profiling): Refactor and add unit tests

--- config/index.js
+++ config/index.js
@@ -477,6 +477,13 @@ var conf = convict({
       doc: 'enable security history',
       default: true,
       env: 'SECURITY_HISTORY_ENABLED'
+    },
+    ipProfiling: {
+      enabled: {
+        doc: 'enable ip profiling',
+        default: true,
+        env: 'IP_PROFILING_ENABLED'
+      }
     }
   },
   lastAccessTimeUpdates: {
