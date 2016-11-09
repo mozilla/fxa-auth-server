ab1066672d84a891282f6e1336a18e8cbf244c8c
Vijay Budhram <vbudhram@mozilla.com>
2016-11-09 13:12:42 -0500

    feat(profiling): PR Fixes

--- config/index.js
+++ config/index.js
@@ -480,7 +480,7 @@ var conf = convict({
     },
     ipProfiling: {
       enabled: {
-        doc: 'enable ip profiling, bypass sign-in confirmation if login is coming from a previously verified address.',
+        doc: 'enable ip profiling, bypass sign-in confirmation if login is coming from a previously verified ip address.',
         default: true,
         env: 'IP_PROFILING_ENABLED'
       }
