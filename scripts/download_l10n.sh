d026ba507a170aadf8ac31d398d41965c99cf81e
vladikoff <vlad.filippov@gmail.com>
2017-08-29 16:24:02 -0400

    refactor(l10n): take l10n repo out of node_modules
    Fixes #1678

--- scripts/download_l10n.sh
+++ scripts/download_l10n.sh
@@ -9,8 +9,8 @@ if [ -z "$FXA_L10N_SHA" ]; then
     FXA_L10N_SHA="master"
 fi
 
-DOWNLOAD_PATH="git://github.com/mozilla/fxa-content-server-l10n.git#$FXA_L10N_SHA"
+DOWNLOAD_PATH="https://github.com/mozilla/fxa-content-server-l10n.git -b $FXA_L10N_SHA"
 
 echo "Downloading L10N files from $DOWNLOAD_PATH..."
-# Download L10N using npm
-npm install $DOWNLOAD_PATH
+# Download L10N using git
+git clone --depth=10 $DOWNLOAD_PATH
