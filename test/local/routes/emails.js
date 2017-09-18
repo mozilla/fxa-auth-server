bc683baf3dc3175c15679f9e9159053e3918ea9b
Shane Tomlinson <stomlinson@mozilla.com>
2017-09-18 13:46:43 +0100

    fix(secondary-email): Fixes based off of @vbudhram's feedback
    * Ditch leftover calls to db.account

--- test/local/routes/emails.js
+++ test/local/routes/emails.js
@@ -853,7 +853,7 @@ describe('/recovery_email', () => {
         assert.equal(args.length, 3, 'log.notifyAttachedServices was passed three arguments')
         assert.equal(args[0], 'primaryEmailChanged', 'first argument was event name')
         assert.equal(args[1], mockRequest, 'second argument was request object')
-        assert.equal(args[2].uid, uid, 'third argument was event data with a uid')
+        assert.equal(args[2].uid, mockRequest.auth.credentials.uid, 'third argument was event data with a uid')
         assert.equal(args[2].email, TEST_EMAIL_ADDITIONAL, 'third argument was event data with new email')
       })
         .then(function () {
