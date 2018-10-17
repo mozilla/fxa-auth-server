3301ac45601bc9241442809f552c2061c2ebee6b
vladikoff <vlad.filippov@gmail.com>
2018-10-17 19:17:59 -0400

    feat(oauth): attempt to merge oauth code
    Fixes #2668

--- bin/key_server.js
+++ bin/key_server.js
@@ -177,7 +177,10 @@ function main() {
       })
     }
   })
-  .catch(process.exit.bind(null, 8))
+  .catch((err) => {
+    console.error(err)
+    process.exit(8)
+  })
 }
 
 if (require.main === module) {
